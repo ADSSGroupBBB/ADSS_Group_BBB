b3d5dcf4bb7b01dcb2b4982575ccc5eddf51f4fd
moriy <moriyc@post.bgu.ac.il>
2025-04-18 18:49:46 +0300

    create classes

--- dev/Domain/Product.java
+++ dev/Domain/Product.java
@@ -1,4 +1,12 @@
 package Domain;
 
 public class Product {
+    private String productName;
+    private int productNumber;
+
+    public Product(String ProductName,int ProductNumber) {
+        this.productName=ProductName;
+        this.productNumber=ProductNumber;
+    }
+
 }
--- dev/Domain/Supplier.java
+++ dev/Domain/Supplier.java
@@ -13,7 +13,27 @@ public class Supplier {
     private LinkedList<Days> deliveryDays;
     private LinkedList<Agreement> agreements;
 
-    public Supplier(int supplierNumber,String supplierName,String bankAccount,LinkedList<String> contactNames,String telephone,LinkedList<Days> deliveryDays){
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public void setAgreements(LinkedList<Agreement> agreements) {
+        this.agreements = agreements;
+    }
+
+    public void setDeliveryDays(LinkedList<Days> deliveryDays) {
+        this.deliveryDays = deliveryDays;
+    }
+
+    public void setContactNames(LinkedList<String> contactNames) {
+        this.contactNames = contactNames;
+    }
+
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public Supplier(int supplierNumber, String supplierName, String bankAccount, LinkedList<String> contactNames, String telephone, LinkedList<Days> deliveryDays){
         this.supplierNumber=supplierNumber;
         this.supplierName=supplierName;
         this.bankAccount=bankAccount;
@@ -22,4 +42,11 @@ public class Supplier {
         this.deliveryDays=deliveryDays;
     }
 
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public void setSupplierNumber(int supplierNumber) {
+        this.supplierNumber = supplierNumber;
+    }
 }
