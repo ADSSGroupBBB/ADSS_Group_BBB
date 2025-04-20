31d8eb33fd315cc4975bc83e5b435191f8756d08
Sapir Aharoni <sapiraharoni32@gmail.com>
2025-04-20 22:37:41 +0300

    test

--- dev/Main/Main.java
+++ dev/Main/Main.java
@@ -11,7 +11,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("===========================================");
         System.out.println("  Super-Li Employee Management System");
-        System.out.println("  Version 1.0");
         System.out.println("===========================================");
 
         // Create and start the navigation manager
