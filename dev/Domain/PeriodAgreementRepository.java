c0de918259e66b48f9e0bdbdfbfc2da57ea0685a
moriy <moriyc@post.bgu.ac.il>
2025-05-30 00:44:13 +0300

    part b

--- dev/Domain/PeriodAgreementRepository.java
+++ dev/Domain/PeriodAgreementRepository.java
@@ -1,4 +1,21 @@
 package Domain;
 
+import dto.PeriodAgreementDto;
+
 public interface PeriodAgreementRepository {
+    PeriodAgreementDto savePeriodAgreement (int supplierNumber, String date);
+    void addProPeriodAgreement(int id,int numPro, double price,int catalogNumber,int amountToDiscount,int discount,int amountToDiscount,int discount,int amountToOrder);
+    void deleteProductStandardAgree(int numAgree,int productNumber);
+    void setDateStandardAgreement(int numAgreement,String date);
+    void deleteStandardAgreement(int numSup,int numAgree);
+    boolean existProStandardAgreement(int numP,int numA);
+    boolean existRegularAgree(int sup,int numA) ;
+    QuantityAgreement productFromAgreementByIndex(int numAgree,int numP);
+    int numProductOnAgree(int numAgreement);
+    String printProductOnAgree(int num);
+    void setNumCatalogStandard(int productNumber,int numAgree,int catalogNumber);
+    void setPriceStandardAgreement(int productNumber,int numAgree,double price);
+    void setAmountToDiscountStandardAgreement(int productNumber,int numAgree,int amountToDiscount);
+    void setDiscountStandardAgreement(int productNumber,int numAgree,int discount);
+    boolean existProByStandardSup(int numS,int numP );
 }
--- dev/Domain/StandardAgreementRepository.java
+++ dev/Domain/StandardAgreementRepository.java
@@ -1,16 +1,21 @@
 package Domain;
 
 import dto.AgreementDto;
-import dto.ProductDto;
 
-public interface StandardAgreementRepository {
+public interface   StandardAgreementRepository {
     AgreementDto saveStandardAgreement (int supplierNumber,String date);
     void addProStandardAgreement(int id,int numPro, double price,int catalogNumber,int amountToDiscount,int discount);
      void deleteProductStandardAgree(int numAgree,int productNumber);
      void setDateStandardAgreement(int numAgreement,String date);
-     void setAddressPeriod(int numAgreement,String address);
-     void setContactPhonePeriod(int numAgreement,String ContactPhone);
-
-
-
+     void deleteStandardAgreement(int numSup,int numAgree);
+     boolean existProStandardAgreement(int numP,int numA);
+     boolean existRegularAgree(int sup,int numA) ;
+     QuantityAgreement productFromAgreementByIndex(int numAgree,int numP);
+     int numProductOnAgree(int numAgreement);
+     String printProductOnAgree(int num);
+     void setNumCatalogStandard(int productNumber,int numAgree,int catalogNumber);
+     void setPriceStandardAgreement(int productNumber,int numAgree,double price);
+     void setAmountToDiscountStandardAgreement(int productNumber,int numAgree,int amountToDiscount);
+     void setDiscountStandardAgreement(int productNumber,int numAgree,int discount);
+     boolean existProByStandardSup(int numS,int numP );
     }
