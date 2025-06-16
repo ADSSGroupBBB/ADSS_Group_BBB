c4b7bd367ad64fcafbb8abb1d2e18fc1d216c8fe
Sapir Aharoni <sapiraharoni32@gmail.com>
2025-06-16 14:31:17 +0300

    finally merge !!!

--- dev/util/Database.java
+++ dev/util/Database.java
@@ -1,7 +1,7 @@
 package util;
 
 import java.sql.*;
-
+///
 public class Database {
     public static void createTablesIfNotExist(Connection conn) throws SQLException {
         try (Statement st = conn.createStatement()) {
